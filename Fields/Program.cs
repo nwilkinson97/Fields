b796bd4f2e098c32ed7fd19ba1cfa0cdd7347f32
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-31 11:38:45 -0500

    Added Comments
    Comments are always helpful to make sure you udnerstand the code when you return.

--- Fields/Program.cs
+++ Fields/Program.cs
@@ -4,14 +4,20 @@
     {
         static void Main(string[] args)
         {
+            // Create New Customer
             var customer = new Customer();
 
+            //  Add Orders to the Customer
             customer.Orders.Add(new Order());
             customer.Orders.Add(new Order());
+
+            //  Display Orders in Shopping Cart
             System.Console.WriteLine(customer.Orders.Count);
 
+            //  Promote the Customer
             customer.Promote();
 
+            //  Display Orders in Shopping Cart
             System.Console.WriteLine(customer.Orders.Count);
         }
     }
