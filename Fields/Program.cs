a9e2426b9483fa879f058d9dec746064c72101d1
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-31 11:24:29 -0500

    Creating Customer & Order Class
    Remember from the Constructors Lesson where we talked about inheritance.  Where once constructor can inherit from another.  Here we show another implementation of this.
    
    Note:  Here we could set the field Orders in the Customer class and initiate on same line.  example:  public List<Order> Orders = new List<Order>();  I prefer to do this in the base constructor.  Others would argue against however I prefer this method of implemetnation.

--- Fields/Program.cs
+++ Fields/Program.cs
@@ -1,5 +1,33 @@
-﻿namespace Fields
+﻿using System.Collections.Generic;
+
+namespace Fields
 {
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public Customer(int id): this()
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name): this(id)
+        {
+            this.Name = name;
+        }
+    }
+
+    public class Order
+    {
+
+    }
     class Program
     {
         static void Main(string[] args)
