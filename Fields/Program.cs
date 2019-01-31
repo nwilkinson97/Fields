805982a8256df542b4961f910a9410dc691d914a
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-31 11:36:57 -0500

    Showing the Cart Being Emptied
    Here I write some code to add 2 orders show that the orders were infact added and then finally promote the customer and display the Count again.  Here we see that the cart has infact been emptied.

--- Fields/Program.cs
+++ Fields/Program.cs
@@ -5,7 +5,14 @@
         static void Main(string[] args)
         {
             var customer = new Customer();
+
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+            System.Console.WriteLine(customer.Orders.Count);
+
             customer.Promote();
+
+            System.Console.WriteLine(customer.Orders.Count);
         }
     }
 }
