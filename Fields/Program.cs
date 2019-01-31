1b0c4c6fd8878aff98a32d62f78dc5f3d7ae4267
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-31 11:32:30 -0500

    Moving Customer & Orders Class
    Here we have have used the Quick Actions shortcut to move the Customer and Orders Class.

--- Fields/Program.cs
+++ Fields/Program.cs
@@ -1,38 +1,5 @@
-﻿using System.Collections.Generic;
-
-namespace Fields
+﻿namespace Fields
 {
-    public class Customer
-    {
-        public int Id;
-        public string Name;
-        public List<Order> Orders;
-
-        public Customer()
-        {
-            Orders = new List<Order>();
-        }
-
-        public Customer(int id): this()
-        {
-            this.Id = id;
-        }
-
-        public Customer(int id, string name): this(id)
-        {
-            this.Name = name;
-        }
-
-        public void Promote()
-        {
-            Orders = new List<Order>();
-        }
-    }
-
-    public class Order
-    {
-
-    }
     class Program
     {
         static void Main(string[] args)
